eed7c9a27bb00d67418536403a60c0139360f9af
muladopiaza <mulado123@gmail.com>
2025-12-16 00:15:08 +0500

    Changes

--- utils.py
+++ utils.py
@@ -1,14 +1,6 @@
 from models import LinearRegression , LogisticRegression
 from metrics import evaluate
-class ModelFactory:
-    @staticmethod
-    def create(model_name, **kwargs):
-        if model_name == 'LinearRegression':
-            return LinearRegression(**kwargs)
-        elif model_name == "LogisticRegression":
-            return LogisticRegression(**kwargs)
-        else:
-            raise ValueError(f"Unknown model: {model_name}")
+
 class Pipeline:
     def __init__(self, steps):
         self.steps = steps
